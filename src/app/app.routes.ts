b79fc4f43d6226a16f142354bd741e8f3a8278c9
pya-h <p4ya.gcc@gmail.com>
2024-12-04 20:40:04 +0330

    init project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { MarketListComponent } from './market-list/market-list.component';
+import { MarketDetailComponent } from './market-detail/market-detail.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'markets', component: MarketListComponent },
+  { path: 'market/:id', component: MarketDetailComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
